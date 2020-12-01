6b464a23c9b337e1a9a14845301976aef27e74aa
Simon Israelsson <simonisr94@gmail.com>
2020-12-01 20:41:15 +0100

    Added solution to day 1 problem 2

--- 1/1.py
+++ 1/1.py
@@ -1,23 +1,43 @@
-
-
-
-
+# Read input from text file called input.txt
 def getNumberList(filename):
     return [int(x) for x in open(filename, "r").read().split()]
 
-def solve_1a(list, targetSum):
-    diffList = [0]*targetSum
+# Solution to first problem
+def solve1a(inputList, targetSum):
+    diffList = [0]*(targetSum + 1)
     
-    for x in list:
+    for x in inputList:
+
+        if x >= targetSum:
+            continue
+
         diff = targetSum - x
 
         if diffList[x] == 1:
-            print(diff*x)
-            return
+            return diff*x
 
         diffList[diff] = 1
+    
+    return -1
+
+# Returns a list of differences based on a target sum and a list of values < target sum
+def getDiffList(inputList, targetSum):
+    return [targetSum - x for x in inputList]
+
+# Solution to problem 2
+def solve1b(inputList, targetSum):
+
+    diffList = getDiffList(inputList, targetSum)
+
+    for newSum in diffList:
+        partialAns = solve1a(inputList, newSum)
+
+        if partialAns > 0:
+            return partialAns*(targetSum-newSum)
 
 
+# Answer day 1 problem 1
+print(solve1a(getNumberList("input.txt"), 2020))
 
-
-solve_1a(getNumberList("input.txt"), 2020)
+#Answer day 1 problem 2
+print(solve1b(getNumberList("input.txt"), 2020))
